9b2d2c0082775f493aa0eaf42315eded5629fcf4
ApBaidya <aparajitabaidya2@gmail.com>
2026-03-16 23:33:55 +0100

    made node class

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,35 @@
 //Aparajita Baidya 3.16.2026
+#include "Node.h"
+Node::Node(){
+  right = nullptr;
+  left = nullptr;
+}
+
+Node::~Node(){
+  right = nullptr;
+  left = nullptr;//...for...predictability 
+}
+
+void Node::setR(Node* r){
+  right = r;
+}
+
+void Node::setL(Node* l){
+  left = l;
+}
+
+void Node::setD(int d){
+  data = d;
+}
+
+Node* Node::getR(){
+  return right;
+}
+
+Node* Node::getL(){
+  return left;
+}
+
+int Node::getD(){
+  return data;
+}
--- Node.h
+++ Node.h
@@ -2,3 +2,27 @@
 Aparajita Baidya 3.16.2026
 Binary tree node file
 */
+#ifndef NODE
+#define NODE
+
+using namespace std;
+
+class Node{
+ private:
+  Node* right;
+  Node* left;
+  int data;
+ public:
+  Node();//Perfect life
+  ~Node();//Painful death
+  //sets
+  void setR(Node* r);//set right child
+  void setL(Node* l);//set left child
+  void setD(int d);//set data
+  //gets
+  Node* getR();//returns right child
+  Node* getL();//returns left child
+  int getD();//returns data
+};
+
+#endif //NODE
